1157002effa4fcac80f02aa3330ca8480affe7ca
GiPaolaOrtiz <paogi_ortiz@hotmail.com>
2017-07-04 21:50:12 -0500

    codigo php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Ejemplo</title>
+    </head>
+    <body>
+	 <h1>Prueba con Travis y PHP <h1>
+
+        <?php
+            echo "¡Hola, soy un script de PHP!";
+        ?>
+    </body>
+</html>
