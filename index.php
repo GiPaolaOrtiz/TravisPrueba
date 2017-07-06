b0fc83fbf71dc8a3d5bbc1150b4aac1c74484a3b
JhnFbre <walterfa@outlook.es>
2017-07-06 09:06:34 -0500

    updating index.php

--- index.php
+++ index.php
@@ -6,8 +6,14 @@
     <body>
 	 <h1>Prueba con Travis y PHP <h1>
 
-      <?php
+      
+<?php
+$a = array(2, 4, 6, 8);
+echo "sum(a) = " . array_sum($a) . "\n";
 
+$b = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
+echo "sum(b) = " . array_sum($b) . "\n";
 ?>
+
     </body>
 </html>
